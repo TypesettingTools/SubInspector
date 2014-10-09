90b1c841f0527975a28642a3704f3e1e4c5419cf
Youka <spanknebel.borken@t-online.de>
2014-10-09 08:13:49 +0200

    added windows dll export

--- src/ASSInspector.h
+++ src/ASSInspector.h
@@ -6,12 +6,26 @@
 
 #include <stdint.h>
 
+#ifdef _WIN32
+#	ifdef ASSI_STATIC
+#		define ASSI_EXPORT
+#	else
+#		ifdef BUILD_DLL
+#			define ASSI_EXPORT __declspec(dllexport)
+#		else
+#			define ASSI_EXPORT __declspec(dllimport)
+#		endif
+#	endif // ASSI_STATIC
+#else // Non-windows
+#	define ASSI_EXPORT
+#endif // _WIN32
+
 typedef struct ASSI_State_priv ASSI_State;
 
-uint32_t    assi_getVersion( void );
-ASSI_State* assi_init( int, int );
-void        assi_addHeader( ASSI_State*, const char*, unsigned int );
-void        assi_initEvents( ASSI_State*, unsigned int count );
-void        assi_addEvent( ASSI_State*, const char*, unsigned int, unsigned int );
-int         assi_checkLine( ASSI_State*, const int, const int*, const unsigned int, uint8_t* );
-void        assi_cleanup( ASSI_State* );
+ASSI_EXPORT uint32_t    assi_getVersion( void );
+ASSI_EXPORT ASSI_State* assi_init( int, int );
+ASSI_EXPORT void        assi_addHeader( ASSI_State*, const char*, unsigned int );
+ASSI_EXPORT void        assi_initEvents( ASSI_State*, unsigned int count );
+ASSI_EXPORT void        assi_addEvent( ASSI_State*, const char*, unsigned int, unsigned int );
+ASSI_EXPORT int         assi_checkLine( ASSI_State*, const int, const int*, const unsigned int, uint8_t* );
+ASSI_EXPORT void        assi_cleanup( ASSI_State* );
