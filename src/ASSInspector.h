94dbba2a8899f9b764fe9335fad0c586739dd64d
torque <torque@1>
2014-10-12 19:38:50 -0700

    Clean header.

--- src/ASSInspector.h
+++ src/ASSInspector.h
@@ -7,17 +7,17 @@
 #include <stdint.h>
 
 #ifdef _WIN32
-#	ifdef ASS_INSPECTOR_STATIC
-#		define ASSI_EXPORT
-#	else
-#		ifdef BUILD_DLL
-#			define ASSI_EXPORT __declspec(dllexport)
-#		else
-#			define ASSI_EXPORT __declspec(dllimport)
-#		endif
-#	endif // ASS_INSPECTOR_STATIC
+	#ifdef ASS_INSPECTOR_STATIC
+		#define ASSI_EXPORT
+	#else
+		#ifdef BUILD_DLL
+			#define ASSI_EXPORT __declspec(dllexport)
+		#else
+			#define ASSI_EXPORT __declspec(dllimport)
+		#endif // BUILD_DLL
+	#endif // ASS_INSPECTOR_STATIC
 #else // Non-windows
-#	define ASSI_EXPORT
+	#define ASSI_EXPORT
 #endif // _WIN32
 
 #define ASSI_VERSION 0x000003
@@ -25,9 +25,5 @@
 typedef struct ASSI_State_priv ASSI_State;
 
 ASSI_EXPORT uint32_t    assi_getVersion( void );
-ASSI_EXPORT ASSI_State* assi_init( int, int );
-ASSI_EXPORT int         assi_addHeader( ASSI_State*, const char*, unsigned int );
-ASSI_EXPORT int         assi_initEvents( ASSI_State*, unsigned int count );
-ASSI_EXPORT int         assi_addEvent( ASSI_State*, const char*, unsigned int, unsigned int );
-ASSI_EXPORT int         assi_checkLine( ASSI_State*, const int, const int*, const unsigned int, uint8_t* );
+ASSI_EXPORT ASSI_State* assi_init( int, int, const char*, uint32_t );
 ASSI_EXPORT void        assi_cleanup( ASSI_State* );
