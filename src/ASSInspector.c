f743e27807dd67b0d64d6aaad55e5ade6c9ad9da
torque <torque@1>
2014-10-14 15:29:16 -0700

    Goodbye, findDirty.

--- src/ASSInspector.c
+++ src/ASSInspector.c
@@ -19,7 +19,6 @@ struct ASSI_State_priv{
 };
 
 static void msgCallback( int, const char*, va_list, void* );
-static void findDirty( ASS_Image*, ASSI_Rect* );
 
 static void msgCallback( int level, const char *fmt, va_list va, void *data ) {
 	if ( level < 4 ) {
@@ -124,7 +123,6 @@ int assi_calculateBounds( ASSI_State *state, ASSI_Rect **rects, int32_t *times, 
 		ASS_Image *assImage = ass_render_frame( state->assRenderer, assTrack, times[i], NULL );
 		// ASS_Image is apparently a linked list.
 		while ( assImage ) {
-			findDirty( assImage, rects[i] );
 			assImage = assImage->next;
 		}
 	}
@@ -133,71 +131,3 @@ int assi_calculateBounds( ASSI_State *state, ASSI_Rect **rects, int32_t *times, 
 	return 0;
 }
 
-static void findDirty( ASS_Image *assImage, ASSI_Rect *rect ) {
-	rect->x = assImage->w;
-	rect->y = assImage->h;
-	rect->w = 0;
-	rect->h = 0;
-	// If alpha is not 255, the image is not fully transparent and we need
-	// to check the bitmap blending mask to verify if it is dirty or not.
-	if( 0xFF != (assImage->color & 0xFF) ) {
-		uint8_t *byte = assImage->bitmap,
-		         // Pointer to the last byte in a row.
-		        *endByte;
-
-		               // Pointer to the end of the bitmap so we know when
-		               // to stop.
-		const uint8_t *endOfBitmap    = byte + assImage->h * assImage->stride,
-		               chunkSize      = sizeof(uintptr_t),
-		               // Number of bytes left over in a row that cannot be
-		               // processed as a single chunk.
-		               chunkRemainder = assImage->w % chunkSize;
-
-		               // Padding for alignment. Since the blending mask
-		               // only has 1 byte per pixel, the stride is just the
-		               // width plus whatever padding.
-		const uint16_t padding    = assImage->stride - assImage->w,
-		               // Maximum number of chunks that fit into a row
-		               // without overflow.
-		               chunkWidth = assImage->w / chunkSize;
-
-		           // Pointer to the current chunk that is being processed.
-		uintptr_t *chunk,
-							 // Pointer to the last chunk in a row.
-		          *endChunk;
-
-		int32_t x, y = 0;
-		while ( byte < endOfBitmap ) {
-			x = 0;
-			chunk    = (uintptr_t *)byte;
-			endChunk = chunk + chunkWidth;
-			while ( chunk < endChunk ) {
-				if ( *chunk ) {
-					for ( byte = (uint8_t *)chunk; byte < (uint8_t *)(chunk + chunkSize); byte++ ) {
-						if ( *byte ) {
-							rect->x = (x < rect->x)? x: rect->x;
-							rect->w = (x > rect->x + rect->w)? x - rect->x: rect->w;
-						}
-						x++;
-					}
-				} else {
-					x += chunkSize;
-				}
-				chunk++;
-			}
-			byte = (uint8_t *)chunk;
-			endByte = byte + chunkRemainder;
-			while ( byte < endByte ) {
-				if ( *byte ) {
-					// redundant.
-					rect->x = (x < rect->x)? x: rect->x;
-					rect->w = (x > rect->x + rect->w)? x - rect->x: rect->w;
-				}
-				byte++;
-			}
-			// Increment to the next row.
-			y += 1;
-			byte += padding;
-		}
-	}
-}
