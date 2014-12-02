6ea521929f65b13595a484332f0694e624c8d3db
torque <torque@1>
2014-12-02 15:09:54 -0800

    C++ example: don't segfault if input file doesn't exist.

--- examples/C++/example.cpp
+++ examples/C++/example.cpp
@@ -61,6 +61,7 @@ int main( int argc, char **argv ) {
 	std::fstream inAss( argv[1], std::ifstream::in );
 	if ( inAss.fail( ) ) {
 		std::cout << "Failed to open " << argv[1] << std::endl;
+		return 1;
 	}
 
 	std::string line;
