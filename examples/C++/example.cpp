35a89b3dcd451235829df5bb4f8d65c85b2c7051
torque <torque@1>
2015-09-13 22:06:40 -0700

    C++ example: refactor and fix compilation.

--- examples/C++/example.cpp
+++ examples/C++/example.cpp
@@ -18,26 +18,122 @@ struct DialogLine {
 
 class ScriptWrapper {
 		std::string headerBuffer;
+		std::fstream &fileStream;
 		std::vector<DialogLine> lineBuffers;
 
 	public:
-		ScriptWrapper( void );
+		ScriptWrapper( std::fstream &inFile );
+		~ScriptWrapper( void );
+		void readFile( int &width, int &height );
+		void rewind( void );
 		void appendHeader( std::string line );
-		std::string getHeader( void );
-		std::vector<DialogLine> getLines( void );
+		std::string& getHeader( void );
+		std::vector<DialogLine>& getLines( void );
 		void appendDialogue( std::string line, int time );
 };
 
-ScriptWrapper::ScriptWrapper( void ) :
-headerBuffer( "[Script Info]\n" ) {
+ScriptWrapper::ScriptWrapper( std::fstream &inFile ) :
+headerBuffer( "[Script Info]\n" ), fileStream( inFile ) {
+}
 
+ScriptWrapper::~ScriptWrapper( void ) {
+	fileStream.close( );
+}
+
+void ScriptWrapper::readFile( int &width, int &height ) {
+	std::string line;
+	std::getline( fileStream, line );
+	if ( "[Script Info]" != line ) {
+		// BOM?
+		if ( "[Script Info]" != line.substr( 3 ) ) {
+			std::cout << "The input file does not appear to be valid ASS." << std::endl;
+			goto fail;
+		}
+	}
+
+	// This is pretty terrible.
+	std::cout << "Reading headers." << std::endl;
+	while ( true ) {
+		std::getline( fileStream, line );
+		if ( '[' == line[0] ) {
+			break;
+		}
+		switch ( line[0] ) {
+			case 'S':
+				if ( 0 == line.compare( 0, 22, "ScaledBorderAndShadow:" ) ) {
+					appendHeader( line );
+				} else if ( 0 == line.compare( 0, 11, "ScriptType:" ) ) {
+					appendHeader( line );
+				}
+				break;
+			case 'P':
+				if ( 0 == line.compare( 0, 9, "PlayResX:" ) ) {
+					std::stringstream converter( line.substr( 9 ) );
+					converter >> width;
+					appendHeader( line );
+				} else if ( 0 == line.compare( 0, 9, "PlayResY:" ) ) {
+					std::stringstream converter( line.substr( 9 ) );
+					converter >> height;
+					appendHeader( line );
+				}
+				break;
+			case 'W':
+				if ( 0 == line.compare( 0, 10, "WrapStyle:" ) ) {
+					appendHeader( line );
+				}
+				break;
+		}
+		if ( !fileStream.good( ) ) {
+			goto fail;
+		}
+	}
+
+	appendHeader( "[V4+ Styles]" );
+
+	std::cout << "Reading styles and dialogue." << std::endl;
+	while ( !fileStream.eof( ) ) {
+		if ( !fileStream.good( ) )
+			goto fail;
+
+		std::getline( fileStream, line );
+		switch ( line[0] ) {
+			case 'S':
+				if ( 0 == line.compare( 0, 6, "Style:" ) ) {
+					appendHeader( line );
+				}
+				break;
+			case 'D':
+				if ( 0 == line.compare( 0, 9, "Dialogue:" ) ) {
+					int hours = 0, minutes = 0, seconds = 0, centiseconds = 0;
+					sscanf( line.substr( 9 ).c_str( ), "%*d,%u:%u:%u.%u", &hours, &minutes, &seconds, &centiseconds );
+					int milliseconds = 3600000*hours + 60000*minutes + 1000*seconds + 10*centiseconds;
+					appendDialogue( line, milliseconds );
+				}
+				break;
+		}
+	}
+
+	appendHeader( "[Events]" );
+	return;
+
+fail:
+	fileStream.close( );
+	throw;
+}
+
+void ScriptWrapper::rewind( void ) {
+	fileStream.clear( );
+	fileStream.seekg( 0, std::ios::beg );
+	if ( !fileStream.good( ) ) {
+		std::cout << "WHAt" << std::endl;
+	}
 }
 
 void ScriptWrapper::appendHeader( std::string line ) {
 	headerBuffer += line + "\n";
 }
 
-std::string ScriptWrapper::getHeader( void ) {
+std::string& ScriptWrapper::getHeader( void ) {
 	return headerBuffer;
 }
 
@@ -45,7 +141,7 @@ void ScriptWrapper::appendDialogue( std::string line, int time ) {
 	lineBuffers.push_back( {time, line + '\n'} );
 }
 
-std::vector<DialogLine> ScriptWrapper::getLines( void ) {
+std::vector<DialogLine>& ScriptWrapper::getLines( void ) {
 	return lineBuffers;
 }
 
@@ -58,92 +154,24 @@ int main( int argc, char **argv ) {
 		return 1;
 	}
 
-	std::fstream inAss( argv[1], std::ifstream::in );
+	std::fstream inAss( argv[1], std::ios::in | std::ios::binary );
 	if ( inAss.fail( ) ) {
 		std::cout << "Failed to open " << argv[1] << std::endl;
 		return 1;
 	}
 
-	std::string line;
-	std::getline( inAss, line );
-	if ( "[Script Info]" != line ) {
-		// BOM?
-		if ( "[Script Info]" != line.substr( 3 ) ) {
-			std::cout << "The input file, " << argv[1] << ", does not appear to be valid ASS." << std::endl;
-			inAss.close( );
-			return 1;
-		}
+	ScriptWrapper wrapper( inAss );
+
+	try {
+		wrapper.readFile( width, height );
+	} catch( ... ) {
+		std::cout << "Bad failure is occurred." << std::endl;
+		return 1;
 	}
 
-	ScriptWrapper wrapper;
+	SI_State *state = si_init( width, height, NULL, NULL );
 
-	// This is pretty terrible.
-	while ( true ) {
-		std::getline( inAss, line );
-		if ( '[' == line[0] ) {
-			break;
-		}
-		switch ( line[0] ) {
-			case 'S':
-				if ( 0 == line.compare( 0, 22, "ScaledBorderAndShadow:" ) ) {
-					wrapper.appendHeader( line );
-				} else if ( 0 == line.compare( 0, 11, "ScriptType:" ) ) {
-					wrapper.appendHeader( line );
-				}
-				break;
-			case 'P':
-				if ( 0 == line.compare( 0, 9, "PlayResX:" ) ) {
-					std::stringstream converter( line.substr( 9 ) );
-					converter >> width;
-					wrapper.appendHeader( line );
-				} else if ( 0 == line.compare( 0, 9, "PlayResY:" ) ) {
-					std::stringstream converter( line.substr( 9 ) );
-					converter >> height;
-					wrapper.appendHeader( line );
-				}
-				break;
-			case 'W':
-				if ( 0 == line.compare( 0, 10, "WrapStyle:" ) ) {
-					wrapper.appendHeader( line );
-				}
-				break;
-		}
-		if ( !inAss.good( ) ) {
-			break;
-		}
-	}
-
-	wrapper.appendHeader( "[V4+ Styles]" );
-
-	while ( true ) {
-		std::getline( inAss, line );
-		switch ( line[0] ) {
-			case 'S':
-				if ( 0 == line.compare( 0, 6, "Style:" ) ) {
-					wrapper.appendHeader( line );
-				}
-				break;
-			case 'D':
-				if ( 0 == line.compare( 0, 9, "Dialogue:" ) ) {
-					int hours = 0, minutes = 0, seconds = 0, centiseconds = 0;
-					sscanf( line.substr( 9 ).c_str( ), "%*d,%u:%u:%u.%u", &hours, &minutes, &seconds, &centiseconds );
-					int milliseconds = 3600000*hours + 60000*minutes + 1000*seconds + 10*centiseconds;
-					wrapper.appendDialogue( line, milliseconds );
-				}
-				break;
-		}
-		if ( !inAss.good( ) ) {
-			break;
-		}
-	}
-
-	inAss.close( );
-
-	wrapper.appendHeader( "[Events]" );
-
-	ASSI_State *state = si_init( width, height, NULL, NULL );
-
-	std::string header = wrapper.getHeader( );
+	auto header = wrapper.getHeader( );
 	si_setHeader( state, header.c_str( ), header.length( ) );
 
 	std::map<uint32_t,int> hashes;
@@ -154,7 +182,7 @@ int main( int argc, char **argv ) {
 
 	for ( const auto &pair : wrapper.getLines( ) ) {
 		si_setScript( state, pair.line.c_str( ), pair.line.length( ) );
-		ASSI_Rect rect{ 0, 0, 0, 0, 0 };
+		SI_Rect rect{ 0, 0, 0, 0, 0 };
 		si_calculateBounds( state, &rect, &pair.time, 1 );
 		++processed;
 		match = hashes.find( rect.hash );
